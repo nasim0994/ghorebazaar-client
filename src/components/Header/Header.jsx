be06bc35a2c4a9a75dd09ff73182bbe6d60b5f8e
nasim0994 <nasimuddin0994@gmail.com>
2023-01-25 20:47:46 +0600

    header login form create

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "./Header.css";
 import { Link } from "react-router-dom";
 import {
   FaFacebookF,
@@ -13,6 +14,21 @@ import { BiSearchAlt2 } from "react-icons/bi";
 import { TbCurrencyTaka } from "react-icons/tb";
 
 const Header = () => {
+  const [fixedForm, setFixedForm] = useState(false);
+
+  useEffect(() => {
+    document.onclick = function (e) {
+      if (
+        e.target.id !== "fixedFormInput1" &&
+        e.target.id !== "fixedFormInput2"
+      ) {
+        setFixedForm(false);
+      }
+    };
+  }, []);
+
+  console.log(fixedForm);
+
   return (
     <header>
       {/* Top Header */}
@@ -93,13 +109,79 @@ const Header = () => {
             </div>
             {/* End/Cart */}
             <div className="flex gap-4 items-center">
-              <div className="hidden lg:block">
+              <div className="hidden lg:block header-signin relative">
                 <Link
                   to=""
-                  className="text-neutral uppercase text-[15px] font-medium hover:text-neutral-content"
+                  className="text-neutral uppercase text-[15px] font-medium hover:text-neutral-content py-5  "
                 >
                   Login/Register
                 </Link>
+
+                {/* Hover SignIn Form */}
+                <div
+                  className={`${
+                    fixedForm ? "account-fixed-form" : "account-form"
+                  } shadow`}
+                >
+                  <form className="p-5">
+                    <div className="flex justify-between border-b pb-2">
+                      <h2 className="text-lg font-bold ">SIGN IN</h2>
+                      <Link className="text-[#2a3382] text-sm hover:underline">
+                        Create an Account
+                      </Link>
+                    </div>
+                    <div className="py-3">
+                      <label htmlFor="email" className="pb-3">
+                        <span className="text-neutral"> Email </span>
+                        <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        type="email"
+                        name="email"
+                        className="border w-full outline-none px-2 py-1"
+                        id="fixedFormInput1"
+                        required
+                        onClick={() => setFixedForm(true)}
+                      />
+                    </div>
+                    <div className="pb-3">
+                      <label htmlFor="password" className="pb-3">
+                        <span className="text-neutral"> Password </span>
+                        <span className="text-red-600">*</span>
+                      </label>
+                      <input
+                        type="password"
+                        name="password"
+                        className="border w-full outline-none px-2 py-1"
+                        id="fixedFormInput2"
+                        required
+                        onClick={() => setFixedForm(true)}
+                      />
+                    </div>
+                    <button className="w-full bg-primary text-base-100 py-1 hover:bg-secondary duration-300">
+                      SIGN IN
+                    </button>
+                    <div className="flex justify-between pt-3">
+                      <div>
+                        <input
+                          type="checkbox"
+                          name="remember"
+                          id="remember"
+                          className="cursor-pointer"
+                        />
+                        <label
+                          htmlFor="remember"
+                          className="pl-2 cursor-pointer"
+                        >
+                          Remember me
+                        </label>
+                      </div>
+                      <Link className="text-[#2a3382] text-sm hover:underline">
+                        Lost your password?
+                      </Link>
+                    </div>
+                  </form>
+                </div>
               </div>
               <div className="flex gap-4 items-center">
                 <button className="text-xl hover:text-neutral-content">
